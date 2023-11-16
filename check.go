020d5fdbe54521a18edc1f3e7573cdd60cb1fe59
constantitus <constantitus@gmail.com>
2023-11-16 20:56:00 +0200

    fixed username not being set when logging from cookies

--- check.go
+++ check.go
@@ -10,28 +10,30 @@ import (
 
 func CheckCredentials() bool {
     if username != "" {
-        return Check(username, password, false)
+        return Check(false)
     }
     if cookies.user != "" {
-        return Check(cookies.user, cookies.pass, true)
+        username = cookies.user
+        password = cookies.pass
+        return Check(true)
     }
     return false
 }
 
-func Check(user string, pass string, hashed bool) bool {
-    cred_file, err := os.ReadFile(UserDir + user + ".txt")
+func Check(cookie bool) bool {
+    cred_file, err := os.ReadFile(UserDir + username + ".txt")
     if err == nil {
         file_string := strings.TrimSuffix(string(cred_file), "\n")
-        if hashed && pass == file_string {
+        if cookie && password == file_string {
             return true
         } else {
             tmp := sha256.New()
-            tmp.Write([]byte(pass))
-            pass = hex.EncodeToString(tmp.Sum(nil))
-            if pass == file_string {
+            tmp.Write([]byte(password))
+            hashed_pass := hex.EncodeToString(tmp.Sum(nil))
+            if hashed_pass == file_string {
                 if remember {
-                    fmt.Printf("Set-Cookie: username=%s\n", user)
-                    fmt.Printf("Set-Cookie: passhash=%s\n", pass)
+                    fmt.Printf("Set-Cookie: username=%s\n", username)
+                    fmt.Printf("Set-Cookie: passhash=%s\n", hashed_pass)
                 }
                 return true
             }
